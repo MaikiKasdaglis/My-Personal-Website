e9ab5b62570aef1b446c7aadbe2b8007bee37039
Michael Kasdaglis <maikikasdaglis@gmail.com>
2023-05-24 12:48:23 -0600

    adds: name, photo, bio, projects, and contat info

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2); 
+const contactInfo = document.createElement("contactInfo");
+contactInfo.textContent = "This content added by JavaScript";
+document.querySelector(p).appendChild(contactInfo); 
